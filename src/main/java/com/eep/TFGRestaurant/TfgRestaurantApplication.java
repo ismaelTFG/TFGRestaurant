c12b723ce73bbf6778013a3c0607b381f0a175c4
MONICO SIENES Ismael <ismael.monico@soprasteria.com>
2022-04-12 13:11:17 +0200

    primer commit

---
+++ src/main/java/com/eep/TFGRestaurant/TfgRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.eep.TFGRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eep/TFGRestaurant/TfgRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eep.TFGRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
